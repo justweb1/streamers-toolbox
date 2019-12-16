59bbac8db34157f23a4b0f2ac609194db9ddf7a5
Justin Weberg <justin.weberg@gmail.com>
2019-12-16 01:27:52 -0600

    add module & setting interface

--- modules/utils/interfaces.ts
+++ modules/utils/interfaces.ts
@@ -3,3 +3,18 @@ export interface Account {
   token: string;
   platform: 'twitch' | 'mixer' | 'discord' | 'twitter';
 }
+
+interface Setting {
+  name: string;
+  help?: string;
+  type: 'input' | 'textbox' | 'toggle';
+  options?: Array<string>;
+  value?;
+}
+
+export interface Module {
+  name: string;
+  uuid: string;
+  description: string;
+  settings: Array<Setting>;
+}
