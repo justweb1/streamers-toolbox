d0a12b57fe517e3b48b9665c1f3347d4aa6b4f54
Justin Weberg <justin.weberg@gmail.com>
2019-12-15 23:35:13 -0600

    fix missing semicolon

--- modules/utils/utils.ts
+++ modules/utils/utils.ts
@@ -1,6 +1,6 @@
 const args = process.argv.includes('--serve'); // slice(1);
 // const serve = args.some(val => val === '--serve');
-console.log('args')
+console.log('args');
 
 
 export const getAngularUrl = (url) => {
